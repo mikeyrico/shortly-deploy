ed25423ddd4bd4b3feee5eb2e1c2d5b6ee8082cb
Hack Reactor Students <accounts@hackreactor.com>
2016-03-31 18:23:31 -0700

    Refactor to include mongo

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,29 @@
-var db = require('../config');
+var db = require('../config');  // opens up connection to database
+var mongoose = require('mongoose');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var linkSchema = mongoose.Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+  link: String
 });
+
+var Link = mongoose.model('Link', linkSchema);  // constructor
+
+var creatSha = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+linkSchema.pre('save', function(next) {
+  var code = createSha(this.url);
+  this.code = code;
+  next();
+});
+    
 
 module.exports = Link;
